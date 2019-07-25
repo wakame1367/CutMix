f7785a61f2f446ccfaaafb825c906a19c1ee1a9f
kot.yuhara <kot.yuhara@trifort.jp>
2019-07-25 12:35:53 +0900

    remove generator

--- cifar10.py
+++ cifar10.py
@@ -5,57 +5,6 @@ from tensorflow.python.keras.datasets import cifar10
 from tensorflow.python.keras.utils import to_categorical
 
 
-class CutMixGenerator:
-    def __init__(self, X_train, y_train, batch_size=32, alpha=0.2,
-                 shuffle=True, data_gen=None):
-        self.X_train = X_train
-        self.y_train = y_train
-        self.batch_size = batch_size
-        self.alpha = alpha
-        self.shuffle = shuffle
-        self.sample_num = len(X_train)
-        self.data_gen = data_gen
-
-    def __call__(self):
-        while True:
-            indexes = self.__get_exploration_order()
-            itr_num = int(len(indexes) // (self.batch_size * 2))
-
-            for i in range(itr_num):
-                batch_ids = indexes[i * self.batch_size * 2:(i + 1) * self.batch_size * 2]
-                X, y = self.__data_generation(batch_ids)
-
-                yield X, y
-
-    def __get_exploration_order(self):
-        indexes = np.arange(self.sample_num)
-
-        if self.shuffle:
-            np.random.shuffle(indexes)
-
-        return indexes
-
-    def __data_generation(self, batch_ids):
-        _, h, w, c = self.X_train.shape
-        _, class_num = self.y_train.shape
-        X1 = self.X_train[batch_ids[:self.batch_size]]
-        X2 = self.X_train[batch_ids[self.batch_size:]]
-        y1 = self.y_train[batch_ids[:self.batch_size]]
-        y2 = self.y_train[batch_ids[self.batch_size:]]
-        l = np.random.beta(self.alpha, self.alpha, self.batch_size)
-        X_l = l.reshape(self.batch_size, 1, 1, 1)
-        y_l = l.reshape(self.batch_size, 1)
-
-        X = X1 * X_l + X2 * (1 - X_l)
-        y = y1 * y_l + y2 * (1 - y_l)
-
-        if self.data_gen:
-            for i in range(self.batch_size):
-                X[i] = self.data_gen.random_transform(X[i])
-
-        return X, y
-
-
 def is_channel_last(image):
     channel = image.shape[2]
     assert len(image.shape) == 3
